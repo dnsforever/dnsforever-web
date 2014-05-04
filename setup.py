ae5a47d6976537cea760266906b1af364ff6f9f2
yjroot <yjroot@gmail.com>
2014-05-05 03:23:51 +0900

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='dnsserver',
+    version='0.1',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=['Flask>=0.10.1',
+                      'Flask-Script>=2.0.3',
+                      'Werkzeug>=0.7',
+                      'Jinja2>=2.4',
+                      'SQLAlchemy>=0.9.4']
+)

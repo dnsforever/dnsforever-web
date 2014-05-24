3406876ad6977ea471443c5c548fc1d53b8cdc00
yjroot <yjroot@gmail.com>
2014-05-25 00:31:20 +0900

    Print each record count to domain list.

--- dnsforever/web/domain.py
+++ dnsforever/web/domain.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, g, render_template, request, url_for, redirect
+from sqlalchemy.sql.expression import true
+from sqlalchemy.sql import func
+
 from dnsforever.web.tools.session import login, get_user
-from dnsforever.models import Domain
+from dnsforever.models import Domain, RecordA, RecordAAAA, RecordCNAME
+from dnsforever.models import RecordMX, RecordTXT
 import re
 
 DOMAIN_PATTERN = re.compile('^([a-z0-9\-]+\.)+([a-z0-9\-]+)$')
@@ -11,7 +15,37 @@ app = Blueprint('domain', __name__, url_prefix='/domain')
 @app.route('/')
 @login(True, '/')
 def index():
-    domain_list = g.domain_list
+    count = lambda type, label: g.session\
+                                 .query(type.domain_id,
+                                        func.count(type.id).label(label))\
+                                 .group_by(type.domain_id).subquery()
+
+    count_a = count(RecordA, 'count_a')
+    count_ddns = g.session.query(RecordA.domain_id,
+                                 func.count(RecordA.id).label('count_ddns'))\
+                          .group_by(RecordA.domain_id)\
+                          .filter(RecordA.ddns == true()).subquery()
+    count_aaaa = count(RecordAAAA, 'count_aaaa')
+    count_cname = count(RecordCNAME, 'count_cname')
+    count_mx = count(RecordMX, 'count_mx')
+    count_txt = count(RecordTXT, 'count_txt')
+
+    domain_list = g.session\
+                   .query(Domain.domain.label('domain'),
+                          count_a.c.count_a,
+                          count_ddns.c.count_ddns,
+                          count_aaaa.c.count_aaaa,
+                          count_cname.c.count_cname,
+                          count_mx.c.count_mx,
+                          count_txt.c.count_txt)\
+                   .outerjoin(count_a)\
+                   .outerjoin(count_ddns)\
+                   .outerjoin(count_aaaa)\
+                   .outerjoin(count_cname)\
+                   .outerjoin(count_mx)\
+                   .outerjoin(count_txt)\
+                   .filter(Domain.owner == get_user()).all()
+
     return render_template('domain.html', domain_list=domain_list)
 
 

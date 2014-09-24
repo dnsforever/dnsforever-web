006eb91527cc52861adeeb97c88f9acdfd83ffde
yjroot <yjroot@gmail.com>
2014-09-24 21:15:17 +0900

    Added ddns api. #14

--- dnsforever/web/apis.py
+++ dnsforever/web/apis.py
@@ -1,9 +1,10 @@
 import string
 
-from flask import Blueprint, g
+from flask import Blueprint, g, request
 from flask.ext import restful
+from flask.ext.restful import reqparse
 
-from dnsforever.models import Domain, DomainOwnership, Record
+from dnsforever.models import Domain, Record, RecordDDNS_A
 
 app = Blueprint('apis', __name__, url_prefix='/apis')
 api = restful.Api(app)
@@ -32,3 +33,39 @@ class ServerUpdate(restful.Resource):
         return result
 
 api.add_resource(ServerUpdate, '/server/update')
+
+
+class DdnsUpdate(restful.Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('key', type=str, required=True)
+        parser.add_argument('host', type=str, required=True)
+        parser.add_argument('ip', type=str)
+        args = parser.parse_args()
+
+        record = g.session.query(RecordDDNS_A)\
+                          .filter(RecordDDNS_A.ddns_key == args['key'])\
+                          .first()
+
+        if not record:
+            return 'ERROR', 404
+
+        if record.name:
+            host = '%s.%s' % (record.name, record.domain.name)
+        else:
+            host = record.domain.name
+
+        if host != args['host']:
+            return 'ERROR', 404
+
+        if 'ip' in args:
+            record.ip = args['ip']
+        else:
+            record.ip = request.remote_addr
+
+        with g.session.begin():
+            g.session.add(record)
+
+        return 'OK'
+
+api.add_resource(DdnsUpdate, '/ddns')
